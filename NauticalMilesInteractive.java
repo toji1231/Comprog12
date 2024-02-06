893c4121211c0b002f2379b88579f9231f52421e
toji1231 <154115399+toji1231@users.noreply.github.com>
2024-02-06 20:44:58 +0800

    Update NauticalMilesInteractive.java

--- NauticalMilesInteractive.java
+++ NauticalMilesInteractive.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
+
 /**
  * NauticalMilesInteractive[Brint Kyle P. De Leon].java
  */
-class NauticalMilesInteractive
-{
-    public static void main(String[] args) 
-    {
+class NauticalMilesInteractive {
+    public static void main(String[] args) {
         double KiloMetersInNauticalMile = 1.852;
         double MilesInNauticalMile = 1.150779;
-        
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of nautical miles: ");
+
+        System.out.print("Enter the number of nautical miles: ");
+
         double nauticalMiles = scanner.nextDouble();
-        
+
         double kilometers = nauticalMiles * KiloMetersInNauticalMile;
         double miles = nauticalMiles * MilesInNauticalMile;
+
+        System.out.println("Nautical miles in kilometers: " + kilometers);
+        System.out.println("Nautical miles in miles: " + miles);
         
-        System.out.println("Nautical miles: " + kilometers + " kilometers.");
-        System.out.println("Nautical miles: " + miles + " miles.");
+        scanner.close();
     }
 }
